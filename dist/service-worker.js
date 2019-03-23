ea50d0d9c3acb8d877d585b9af059e7e2e57c88e
Dmitrij Papulovskij <graycraft@pm.me>
2019-03-23 17:09:51 +0500

    deploy(app)

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/memory-game/dist/precache-manifest.332969268228413075399f96372d8fdf.js"
+  "/memory-game/dist/precache-manifest.e2b41793b9e276456e5078fe70b5a4c6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "memory-game"});
